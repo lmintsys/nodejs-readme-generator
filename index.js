6155316022145ddc5fa7a90e15eefbc43263f977
lmintsys <lubmin23@gmail.com>
2023-02-27 21:27:59 -0500

    added questions array

--- index.js
+++ index.js
@@ -3,7 +3,57 @@ const inquirer = require("inquirer");
 const markDown = require("./utils/generateMarkdown.js");
 const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "What is your email?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Describe your probject in a few sentences",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What commands should be run to install dependencies?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Provide instructions and examples for use",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "What does the user need to know about contributing to repo?",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message: "What command should be run to run tests?",
+    name: "tests",
+  },
+  {
+    type: "list",
+    message: "License: What kind of license should your project have?",
+    name: "license",
+    choices: ["MIT", "Apache", "GPL 3", "BSD", "None"],
+    filter(val) {
+      return val.toLowerCase().replace(/\s/g, "");
+    },
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
