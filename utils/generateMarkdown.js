74c92ef08e5003ec26c9b0f8f2cc5b1abb7ee7f7
lmintsys <lubmin23@gmail.com>
2023-02-27 21:25:23 -0500

    added license links function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+function renderLicenseBadge(license) {
   const badges = {
     mit: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
     apache:
@@ -9,10 +10,21 @@
     none: "",
   };
   return badges[license];
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenseLinks = {
+    mit: "[MIT](https://opensource.org/licenses/MIT)",
+    apache:
+      "[Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    gpl3: "[GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    bsd: "[BSD](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+    none: "",
+  };
+  return licenseLinks[license];
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
